d94f6982e387ae5c12f7fe5ecb7d33e36c74f744
hellfiresong <hellfiresong.me@gmail.com>
2016-01-17 23:17:01 +0800

    Binnary tree structure.

---
+++ binary_tree.c
@@ -0,0 +1,54 @@
+//////////////////////////////////////////////////////////////////////
+//
+//  Binary Tree.
+//  It's very efficien in insert, search, delete operations.
+//
+//  hellfire(asyncloading#163.com)
+//  Jan 17th, 2016
+//////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+
+/**
+ * Binary tree data structure.
+ */
+struct binary_tree {
+  int data;                          /* Data field*/
+  struct binary_tree *left;          /* Left child*/
+  struct binary_tree *right;         /* Right child*/
+};
+
+
+typedef binary_tree node;
+
+
+/**
+ * Insert operation
+ */
+void insert()
+{
+
+}
+
+/**
+ * Search operation
+ */
+void search()
+{
+
+}
+
+/**
+ * Delete operation
+ */
+void delete()
+{
+
+}
+
+/* Do some test case here.*/
+int main(int argc, char** argv)
+{
+
+}
